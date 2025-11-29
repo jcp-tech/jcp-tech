857fb8020764c7d011e3c4dac5afe23b199c8f0c
jcp-tech <jonathan.chacko@maritimesupports.com>
2025-11-29 08:29:32 -0500

    Remove debug scripts for Pygments rendering
    Deleted debug_brackets.py, debug_comment.py, and debug_pygments.py from the RENDER directory. These scripts were used for testing Pygments syntax highlighting and are no longer needed.

--- RENDER/debug_brackets.py
+++
@@ -1,8 +0,0 @@
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-code = "print('hello')\nmy_list = [1, 2]\nmy_dict = {'a': 1}\nmy_tuple = (1, 2)"
-formatter = HtmlFormatter(nowrap=True)
-html = highlight(code, PythonLexer(), formatter)
-print(html)
--- RENDER/debug_comment.py
+++
@@ -1,8 +0,0 @@
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-code = "# This is a comment"
-formatter = HtmlFormatter(nowrap=True)
-html = highlight(code, PythonLexer(), formatter)
-print(html)
